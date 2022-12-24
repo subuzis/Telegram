3f4ae3c7089cae07d454fb3c7c735e107b54f763
Davi Lobato <31938610+subuzis@users.noreply.github.com>
2022-12-24 17:39:01 -0300

    Update telegram_sendmessage.py

--- telegram_sendmessage.py
+++ telegram_sendmessage.py
@@ -1,15 +1,9 @@
-###https://blogs.oracle.com/lad-cloud-experts-pt/post/oracle-funtions---mandando-mensagem-para-o-telegram
-
-####https://api.telegram.org/bot5798237105:AAFXUS4RahSHZhbvSD6NyRoza59VN3c9bzA/getUpdates
-
-###https://www.youtube.com/watch?v=7s7Ng-jBGag
-
 import requests
 
     
 ###Telegram informations
-token = '5798237105:AAFXUS4RahSHZhbvSD6NyRoza59VN3c9bzA'  ###Token BOT
-chat_id = '-868572748'  ###chat ID
+token = '<TOKEN DO BOT'  ###Token BOT
+chat_id = '<CHAT ID'  ###chat ID
 message = 'testando envio de mensagem cavalo'
 URL = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text='+message
 resposta = requests.get(URL)    
