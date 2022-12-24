b9a0bc04532a14461bb28e8896119b8d1bd12e4f
Davi Lobato <31938610+subuzis@users.noreply.github.com>
2022-12-24 17:39:24 -0300

    Update telegram_sendmessage.py

--- telegram_sendmessage.py
+++ telegram_sendmessage.py
@@ -4,7 +4,7 @@ import requests
 ###Telegram informations
 token = '<TOKEN DO BOT'  ###Token BOT
 chat_id = '<CHAT ID'  ###chat ID
-message = 'testando envio de mensagem cavalo'
+message = 'testando envio de mensagem' ##sua mensagem
 URL = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text='+message
 resposta = requests.get(URL)    
 print(resposta.json())
