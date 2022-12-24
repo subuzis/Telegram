cf18d239dabdddc1780a84b6e65ccfcea01a710e
Davi Lobato <31938610+subuzis@users.noreply.github.com>
2022-12-24 17:57:33 -0300

    Create telegram_senddoc.py

---
+++ telegram_senddoc.py
@@ -0,0 +1,12 @@
+###código para enviar arquivos pelo Telegram
+
+import telepot
+    
+###Telegram informations
+token = '<TOKEN DO BOT'  ###Token BOT
+chat_id = '<CHAT ID'  ###chat ID
+
+###Enviar arquivo
+bot = telepot.Bot(token)
+file="screenshot.png"  ##nesse caso o arquivo está no mesmo diretório do código
+bot.sendDocument(chat_id=chat_id, document=open(file, 'rb'))
